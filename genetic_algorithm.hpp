00d5719a755648ba4509cf52f041be70f39952df
Hvarz <hvarzahar@gmail.com>
2020-11-10 13:46:12 +0300

    add new Algorithm

--- genetic_algorithm.hpp
+++ genetic_algorithm.hpp
@@ -31,10 +31,12 @@ struct Point {
         fit = function(x, y);
     }
 
-    auto operator<(const Point& point) const -> bool {
-        return fit < point.fit;
-    }
 };
+
+auto SortFunc(const Point& point1, const Point& point2) {
+    return point1.fit > point2.fit;
+}
+
 
 auto random_values(const double x1, const double x2, const double y1, const double y2) -> Point {
     if (x1 >= x2 || y1 >= y2) throw std::invalid_argument("Invalid segment");
@@ -68,22 +70,50 @@ void Algorithm() {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> prob(0, 1);
-
+    std::uniform_real_distribution<double> dis_mut(-5, 5);
 
     auto population = fill_population(0, 2, -2, 2);
-    std::sort(population.begin(), population.end());
+    std::sort(population.begin(), population.end(), SortFunc);
     print_results(population);
 
-
     for (size_t i = 0; i < generations; i++) {
-        for (size_t j = 0; j < population.size(); j++) {
+        for (auto &point : population) {
             auto p = prob(gen);
             if (p < probability) {
-
+                point.x = fmod(point.x * dis_mut(gen), 1);
+                point.y = fmod(point.y * dis_mut(gen), 1);
+                point.fit = function(point.x, point.y);
             }
         }
-    }
+        std::sort(population.begin(), population.end(), SortFunc);
+        size_t buf = 0;
+        std::vector<Point> new_population(4);
+        if (population[0].fit != population[1].fit) {
+            buf = 1;
+        } else {
+            buf = 2;
+        }
 
+        new_population[0].x = population[0].x;
+        new_population[0].y = population[buf].y;
+        new_population[0].fit = function(new_population[0].x, new_population[0].y);
+
+        new_population[1].x = population[buf].x;
+        new_population[1].y = population[0].y;
+        new_population[1].fit = function(new_population[1].x, new_population[1].y);
+
+        new_population[2].x = population[0].x;
+        new_population[2].y = population[buf + 1].y;
+        new_population[2].fit = function(new_population[2].x, new_population[2].y);
+
+        new_population[3].x = population[buf + 1].x;
+        new_population[3].y = population[0].y;
+        new_population[3].fit = function(new_population[3].x, new_population[3].y);
+
+        std::sort(new_population.begin(), new_population.end(), SortFunc);
+        std::cout << "Iteration # " << i + 1 << std::endl;
+        print_results(new_population);
+    }
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 
 #include "genetic_algorithm.hpp"
 
-
-
-
 int main() {
-    auto population = fill_population(0, 2, -2, 2);
-    std::sort(population.begin(), population.end());
-    print_results(population);
+    Algorithm();
     return 0;
 }
